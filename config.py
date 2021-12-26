28bfabfd1c4cb8f59458b5d1385d470176264f65
Forevka <zebestforevka@gmail.com>
2021-12-26 11:25:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ api_logs_filename = "api_logger.log"
 discord_logs_filename = "discord_logger.log"
 
 discord_channel_for_notifiers = 923165182581174284
-discord_bot_token = "OTIzMTM3MjQ5NDM1MDY2Mzc5.YcLojw.xQKqwewgC9EVSM0w6Kavw1oONNQ"
+discord_bot_token = ""
 discord_notification_points = {
     3: "*Внимание!*\nСервер перезапустится через 3 минуты",
     1: "*Внимание!*\nСервер перезапустится через 1 минуту",
